e847b2a9934dbf529dc598650f3fe2ea241f863f
Annie1790 <budayaniko@outlook.com>
2024-06-18 17:45:21 +0100

    functional programming: modify GildedRose to call new functional solution

--- app/src/main/java/gildedrose/GildedRose.java
+++ app/src/main/java/gildedrose/GildedRose.java
@@ -1,65 +1,93 @@
 package gildedrose;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class GildedRose {
     List<Item> items;
+    Map<String, Strategy> itemStrategies = new HashMap<>();
+
+    final String BRIE = "Aged Brie";
+    final String PASS = "Backstage passes to a Coldplay concert";
+    final String EGG = "Fabergé egg";
 
     public GildedRose(List<Item> items) {
         this.items = items;
     }
 
+    public void applyStrategy() {
+        itemStrategies.put(BRIE, new BrieStrategy());
+        itemStrategies.put(EGG, new FabergeStrategy());
+        itemStrategies.put(PASS, new PassStrategy());
+        items = items
+                .stream()
+                .map(item -> Map.entry(item, findItemWithStrategy(item)))
+                .map(itemStrategyEntry ->
+                        itemStrategyEntry.getValue().handle(
+                                itemStrategyEntry.getKey()
+                        ))
+                .toList();
+    }
+
+    public Strategy findItemWithStrategy(Item item) {
+        return (itemStrategies.containsKey(item.name))
+                ? itemStrategies.get(item.name)
+                : new AnyItemStrategy();
+    }
+
     public void updateQuality() {
-        for (int i = 0; i < items.size(); i++) {
-            if (!items.get(i).name.equals("Aged Brie")
-                    && !items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
-                if (items.get(i).quality > 0) {
-                    if (!items.get(i).name.equals("Fabergé egg")) {
-                        items.get(i).quality = items.get(i).quality - 1;
-                    }
-                }
-            } else {
-                if (items.get(i).quality < 50) {
-                    items.get(i).quality = items.get(i).quality + 1;
-
-                    if (items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
-                        if (items.get(i).sellIn < 11) {
-                            if (items.get(i).quality < 50) {
-                                items.get(i).quality = items.get(i).quality + 1;
-                            }
-                        }
-
-                        if (items.get(i).sellIn < 6) {
-                            if (items.get(i).quality < 50) {
-                                items.get(i).quality = items.get(i).quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items.get(i).name.equals("Fabergé egg")) {
-                items.get(i).sellIn = items.get(i).sellIn - 1;
-            }
-
-            if (items.get(i).sellIn < 0) {
-                if (!items.get(i).name.equals("Aged Brie")) {
-                    if (!items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
-                        if (items.get(i).quality > 0) {
-                            if (!items.get(i).name.equals("Fabergé egg")) {
-                                items.get(i).quality = items.get(i).quality - 1;
-                            }
-                        }
-                    } else {
-                        items.get(i).quality = items.get(i).quality - items.get(i).quality;
-                    }
-                } else {
-                    if (items.get(i).quality < 50) {
-                        items.get(i).quality = items.get(i).quality + 1;
-                    }
-                }
-            }
-        }
+        applyStrategy();
+//        for (int i = 0; i < items.size(); i++) {
+//            if (!items.get(i).name.equals("Aged Brie")
+//                    && !items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
+//                if (items.get(i).quality > 0) {
+//                    if (!items.get(i).name.equals("Fabergé egg")) {
+//                        items.get(i).quality = items.get(i).quality - 1;
+//                    }
+//                }
+//            } else {
+//                if (items.get(i).quality < 50) {
+//                    items.get(i).quality = items.get(i).quality + 1;
+//
+//                    if (items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
+//                        if (items.get(i).sellIn < 11) {
+//                            if (items.get(i).quality < 50) {
+//                                items.get(i).quality = items.get(i).quality + 1;
+//                            }
+//                        }
+//
+//                        if (items.get(i).sellIn < 6) {
+//                            if (items.get(i).quality < 50) {
+//                                items.get(i).quality = items.get(i).quality + 1;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (!items.get(i).name.equals("Fabergé egg")) {
+//                items.get(i).sellIn = items.get(i).sellIn - 1;
+//            }
+//
+//            if (items.get(i).sellIn < 0) {
+//                if (!items.get(i).name.equals("Aged Brie")) {
+//                    if (!items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
+//                        if (items.get(i).quality > 0) {
+//                            if (!items.get(i).name.equals("Fabergé egg")) {
+//                                items.get(i).quality = items.get(i).quality - 1;
+//                            }
+//                        }
+//                    } else {
+//                        items.get(i).quality = items.get(i).quality - items.get(i).quality;
+//                    }
+//                } else {
+//                    if (items.get(i).quality < 50) {
+//                        items.get(i).quality = items.get(i).quality + 1;
+//                    }
+//                }
+//            }
+//        }
     }
     
    @Override
